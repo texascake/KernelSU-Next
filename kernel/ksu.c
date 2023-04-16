253eb212d45c8dc980025c2d97f4a54186f629fd
RyuujiX <saputradenny712@gmail.com>
2023-04-16 13:01:55 +0700

    kernel: Add Cmdline toggle support
    * To install with or without kernelsu
    
    Change-Id: I21d8b85ffdcb3b745bac4dcd46dc549810a6c062
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -3,6 +3,7 @@
 #include <linux/kobject.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
+#include <linux/init.h>
 
 #include "allowlist.h"
 #include "arch.h"
@@ -14,6 +15,22 @@
 #ifdef CONFIG_KSU_SUSFS
 #include <linux/susfs.h>
 #endif
+
+unsigned int enable_kernelsu = 1;
+
+static int __init read_kernelsu_state(char *s)
+{
+	if (s)
+		enable_kernelsu = simple_strtoul(s, NULL, 0);
+
+	return 1;
+}
+__setup("kernelsu.enabled=", read_kernelsu_state);
+
+unsigned int get_ksu_state(void)
+{
+	return enable_kernelsu;
+}
 
 static struct workqueue_struct *ksu_workqueue;
 
@@ -43,6 +60,12 @@ extern void ksu_ksud_exit();
 
 int __init ksu_kernelsu_init(void)
 {
+
+	if (enable_kernelsu < 1) {
+		pr_info_once(" is disabled");
+		return 0;
+	}
+
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
@@ -82,6 +105,9 @@ int __init ksu_kernelsu_init(void)
 
 void ksu_kernelsu_exit(void)
 {
+	if (enable_kernelsu < 1)
+		return;
+	
 	ksu_allowlist_exit();
 
 	ksu_throne_tracker_exit();
